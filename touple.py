b9a4086db8290fccdf6815020e58158ebd167b62
neeraj.b.sharma <neeraj.b.sharma@rakute.com>
2023-07-09 13:41:20 +0530

    touple

---
+++ touple.py
@@ -0,0 +1,13 @@
+t=(20,30,10,90,50,40)
+print(t)
+# t[2]=123;
+# print(t)
+print(type(t))
+print(t*3)
+print(t.count(20))
+print(t.index(10))
+print(t[2:4])
+l=[67,43,56,76,12,12,34]
+print(type(l))
+t1=tuple(l)
+print(type(t1))
